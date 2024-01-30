1817b25c75ff9a615a568071c0176628ac98def2
Anjali Khandekar <khandekaranjali403@gmail.com>
2024-01-30 12:48:33 +0530

    Backend -task2

---
+++ src/main/java/com/anjali/microservices/currencyconversionservice/CurrencyConversionApplication.java
@@ -0,0 +1,24 @@
+package com.anjali.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * After getting to know about the exchange rate, we need to convert an amount
+ * from a currency to another after communicating with CurrencyExchange service.
+ * So we use this service.
+ * 
+ * @author 
+ *
+ */
+@SpringBootApplication
+@EnableFeignClients
+
+public class CurrencyConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionApplication.class, args);
+	}
+
+}
